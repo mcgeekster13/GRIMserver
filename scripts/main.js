813407c8a59b1f3cd52186f9385e23e50fe5db99
Cody McGee <cfmcgee7@gmail.com>
2022-01-16 17:03:56 -0500

    initial commit of base files
    initial files created for GRIM Control Suite

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+		
+	 $('blue').on('click', function(){
+		 //send command to GRIM to pick up blue cubes		 
+	 });//end blue
+	 
+	 $('red').on('click', function(){
+		 //send command to GRIM to pick up red cubes		 
+	 });//end red
+	 
+	 $('green').on('click', function(){
+		 //send command to GRIM to pick up green cubes		 
+	 });//end green
+	 
+	 $('all').on('click', function(){
+		 //send command to GRIM to pick up all cubes		 
+	 });//end all
+	 
+ 
+		
+}); // end ready
+
